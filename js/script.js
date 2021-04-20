e2dae22d9d4f3eed0076deb28a1ac8a4e4de31fc
Andrea <andrea3aprile99@gmail.com>
2021-04-20 16:48:31 +0200

    risistemazione 4 immagini main

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ new Vue({
   el: '#root',
   data: {
     listNav:['HOME','THE CLUB','EVENTS'],
-    listNavbar:['JOBS','NEWS','CONTACT US']
+    listNavbar:['JOBS','NEWS','CONTACT US'],
+    fotoUno:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg'],
+
   },
  computed: { },
  methods:{
