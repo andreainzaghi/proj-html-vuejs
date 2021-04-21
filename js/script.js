51c73789be32da0e6e96d947937d5301e56dfb56
Andrea <andrea3aprile99@gmail.com>
2021-04-21 18:19:40 +0200

    footer

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ new Vue({
   el: '#root',
   data: {
     listNav:['HOME','THE CLUB','EVENTS'],
+     show: false,
     listNavbar:['JOBS','NEWS','CONTACT US'],
     fotoUno:[
       { date:'img/1.jpg',
@@ -14,7 +15,17 @@ new Vue({
       { date:'img/4.jpg',
       message: 'resident dj'}
     ],
-
+  //   videos: [
+  //   { video:'video/video1.mp4',
+  //   title:'Dj Fusion',
+  //   date:'August 28, 2021 @ 22.00 am'},
+  //   {video:'video/video2.mov',
+  //   title:'House Nation',
+  //   date:'September 9, 2021 @ 22.00 am'},
+  //   {video:'video/video3.mp4',
+  //   title:'VIP Taste Session',
+  //   date:'April 3, 2021 @ 22.00 am'}
+  // ],
     items: [{ img:'img/111.jpg',
     title:'Dj Fusion',
     date:'August 28, 2021 @ 22.00 am'},
@@ -32,11 +43,32 @@ new Vue({
     date:'August 22, 2021 @ 22.00 am'},
     {img:'img/665.jpg',
     title:'Urban Vibes',
-    date:'July 19, 2021 @ 22.00 am'}]
-
+    date:'July 19, 2021 @ 22.00 am'}],
+    currentUser:0
   },
   computed: { },
   methods: {
+    prova:function(index){
+    this.currentUser=index;
+  },
+   //  beforeEnter: function (el) {
+   //   el.style.opacity = 0
+   //   el.style.transformOrigin = 'left'
+   // },
+   // enter: function (el, done) {
+   //   Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
+   //   Velocity(el, { fontSize: '1em' }, { complete: done })
+   // },
+   // leave: function (el, done) {
+   //   Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
+   //   Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
+   //   Velocity(el, {
+   //     rotateZ: '45deg',
+   //     translateY: '30px',
+   //     translateX: '30px',
+   //     opacity: 0
+   //   }, { complete: done })
+   // },
     shuffle: function () {
       let counter = this.items.length;
 
