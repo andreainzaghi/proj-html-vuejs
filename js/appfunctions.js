ca46aae936643672ee0413d82c95c56474fb27a8
Andrea <andrea3aprile99@gmail.com>
2021-04-22 15:47:16 +0200

    aggiunta schermata

--- js/appfunctions.js
+++ js/appfunctions.js
@@ -1,9 +1,36 @@
-// var x = document.getElementById("myVideo");
-//
-// function playVid() {
-//   x.play();
-// }
-//
-// function pauseVid() {
-//   x.pause();
-// }
+const flightPath = {
+  curviness:1.25,
+  autoRotate:true,
+  values:[
+    {x:100,y:-20},
+    {x:300,y:10},
+    {x:500,y:240},
+    {x:750,y:-240},
+    {x:350,y:-50},
+    {x:600,y:100},
+    {x:800,y:0},
+    {x:window.innerWidth, y:-250}
+  ]
+}
+
+const tween = new TimelineLite();
+tween.add(
+  TweenLite.to('.paper-plane', 5,{
+    bezier:flightPath,
+    easy:Power1.easyinOut
+  })
+);
+
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: " .animation",
+  duration:2000,
+  triggerHook:0
+})
+
+.setTween(tween)
+// .addIndicators()
+.setPin(" .animation")
+.addTo(controller);
