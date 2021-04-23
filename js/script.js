41fc6e01f4d6f839bc21c685d44d1e6664544f65
Andrea <andrea3aprile99@gmail.com>
2021-04-23 13:15:47 +0200

    indentazione

--- js/script.js
+++ js/script.js
@@ -49,17 +49,6 @@ fotoUno:[
   { date:'img/4.jpg',
   message: 'resident dj'}
 ],
-//   videos: [
-//   { video:'video/video1.mp4',
-//   title:'Dj Fusion',
-//   date:'August 28, 2021 @ 22.00 am'},
-//   {video:'video/video2.mov',
-//   title:'House Nation',
-//   date:'September 9, 2021 @ 22.00 am'},
-//   {video:'video/video3.mp4',
-//   title:'VIP Taste Session',
-//   date:'April 3, 2021 @ 22.00 am'}
-// ],
 items: [{ img:'img/111.jpg',
 title:'Dj Fusion',
 date:'August 28, 2021 @ 22.00 pm'},
@@ -177,24 +166,6 @@ popDown2:function(){
        this.slideIndex = 0;
      }
    },
-//  beforeEnter: function (el) {
-//   el.style.opacity = 0
-//   el.style.transformOrigin = 'left'
-// },
-// enter: function (el, done) {
-//   Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
-//   Velocity(el, { fontSize: '1em' }, { complete: done })
-// },
-// leave: function (el, done) {
-//   Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
-//   Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
-//   Velocity(el, {
-//     rotateZ: '45deg',
-//     translateY: '30px',
-//     translateX: '30px',
-//     opacity: 0
-//   }, { complete: done })
-// },
 shuffle: function () {
   let counter = this.items.length;
 
